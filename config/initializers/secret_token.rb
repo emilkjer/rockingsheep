24008af76f2d0dcbd9a56d76a8040cc7abe46d19
Emil Kjer <emil@kjer.info>
2012-10-05 21:09:32 +1000

    Init project for heroku

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rockingsheep::Application.config.secret_token = 'caff1d72ddd482e3762f22819ed5b17829679522b8addda39ffb5ef0bf8258b32e74f21d28fb81dc2fb2a90b7676a62906203e3d5cc7ad6fbc652ec02b269aa2'
